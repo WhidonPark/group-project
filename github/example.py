8d42d8a515e839d58b52526025d89d79c41567f8
Whidon Park <whidon@naver.com>
2024-05-02 00:22:10 +0900

    3 commit

--- github/example.py
+++ github/example.py
@@ -16,4 +16,5 @@ starbucks =["아이언맨", "토르", "아이엠 그루트"]
 for customer in starbucks:
     print ("{0}, 커피가 준비되었습니다".format(customer)) 
     # for 다음에는 넣고자 하는것  다음에는 넣고자하는것의 범위  그래서 format 이후에는 대상
+    # for 다음123123123~~~~~~~~~
 
